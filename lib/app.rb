b2d69066d5fa1214598a2d43d12c2522204109c5
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-05-30 00:23:57 +0900

    Modify App

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 Bundler.require
 
+require_relative 'worker'
+
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
   set :server, :puma
@@ -10,6 +12,7 @@ class App < Sinatra::Base
   end
 
   get '/' do
+    Worker.perform_async(params)
     response.status.to_s
   end
 end
