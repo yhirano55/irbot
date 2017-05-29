8d5e0c75391bbb4512bb2aed4cc07ee1d0a9971a
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-05-30 01:18:41 +0900

    Modify App

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,8 @@ require 'bundler/setup'
 Bundler.require
 
 require_relative 'worker'
+require_relative 'runner'
+require_relative 'twitter_api'
 
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
@@ -11,8 +13,23 @@ class App < Sinatra::Base
     enable :logging
   end
 
-  get '/' do
-    Worker.perform_async(params)
-    response.status.to_s
+  SEARCH_WORD = '#rubyinterpreter2017'.freeze
+
+  post '/' do
+    username = format_username(params[:username])
+    text = format_text(params[:text])
+    Worker.perform_async(username: username, text: text)
+    message = format('@%{username} %{status}', username: username, status: response.status.to_s)
+    TwitterApi.update(message)
+  end
+
+  private
+
+  def format_username(value)
+    value.to_s.strip
+  end
+
+  def format_text(value)
+    value.to_s.gsub(/#{SEARCH_WORD}/, '').strip
   end
 end
